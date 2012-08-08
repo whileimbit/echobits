a2966ba0a38fa743faa32826bd80f7e15a36e45d
Jie Ma <jiema8@gmail.com>
2012-08-08 23:43:57 +1000

    move secret token to config file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-EchoBits::Application.config.secret_token = 'e8ae543d76660100ac27da145751399ef1d891ff095d44950ed203f735ac7583e4780731b3b61bade49707be2e30117fba3c3a5e09ce3ba6f5bdb19f24a5cabe'
+EchoBits::Application.config.secret_token = APP_CONFIG['secret_token']
